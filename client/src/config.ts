932f28dcaa6600fa6cc22243150f9a3909871f04
nampt010897 <nam.thanhphan97@gmail.com>
2023-08-23 14:08:22 +0700

    update

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'hbcvr27w41'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-ngx6wjczjbq4hw84.us.auth0.com',           // Auth0 domain
+  clientId: 'VwobvOy2kHdeeN4HSdqg9aRXuo71jE6T',  // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
